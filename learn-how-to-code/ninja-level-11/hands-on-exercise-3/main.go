08375303d1f10c23760c76654299e1e46beb2dbc
Max Li <maxlivinci@gmail.com>
2018-11-06 10:13:09 +0800

    update hands-on-exercise-3

---
+++ learn-how-to-code/ninja-level-11/hands-on-exercise-3/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+type customErr struct {
+	info string
+}
+
+func (ce customErr) Error() string {
+	return fmt.Sprintf("Custom error: %v", ce.info)
+}
+
+func main() {
+	c1 := customErr{
+		info: "Hey wazz up!",
+	}
+
+	foo(c1)
+}
+
+func foo(e error) {
+	fmt.Println(e)
+}
