6853823c9459d164d976936b0c2afb9ae2255bb4
Max Li <maxlivinci@gmail.com>
2018-12-26 12:13:48 +0100

    update tutorial - scope

--- tutorials/basic/scope/scope.go
+++ tutorials/basic/scope/scope.go
@@ -1,3 +1,8 @@
+/*
+	Scope
+		- Scope is the accessibility of variables, functions, and objects in some particular part of your code.
+		- In other words, scope determines the visibility of variables and other resources in areas of your code.
+*/
 package main
 
 import "fmt"
